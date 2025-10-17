aa3bd925d84baf7cb4f46b3899497f1829e8bb29
Pedro-0101 <pedro.tiete@outlook.com>
2025-10-17 20:54:39 -0300

    adicionado components de local e lista

---
+++ projeto-react/src/components/local/Local.tsx
@@ -0,0 +1,20 @@
+type LocalProps = {
+  nome: string
+  imagem: string
+  href?: string
+  descricao: string
+}
+
+function Local({ nome, imagem, href = '#', descricao }: LocalProps) {
+  return (
+    <div className="local">
+      <img src={imagem} alt={`Foto ${nome}`} />
+      <p>
+        <a href={href}>{nome}</a>
+        <p>{descricao}</p>
+      </p>
+    </div>
+  )
+}
+
+export default Local;
