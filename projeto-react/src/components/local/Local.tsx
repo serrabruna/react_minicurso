7f075fe87f4ec29364c2a1b7f379ce02670aff4d
serrabruna <brunaibserra@hotmail.com>
2025-10-18 12:11:43 -0300

    estilização de lista e local

--- projeto-react/src/components/local/Local.tsx
+++ projeto-react/src/components/local/Local.tsx
@@ -9,12 +9,15 @@ type LocalProps = {
 
 function Local({ nome, imagem, href = '#', descricao }: LocalProps) {
   return (
-    <div className="local">
-      <img src={imagem} alt={`Foto ${nome}`} />
-      <p>
-        <a href={href}>{nome}</a>
-        <p>{descricao}</p>
-      </p>
+    <div className="local-card">
+      <img src={imagem} alt={`Foto ${nome}`}  className='local-imagem'/>
+      
+      <div className="local-conteudo">
+        <p>
+          <a href={href}>{nome}</a>
+          <p>{descricao}</p>
+        </p>
+      </div>
     </div>
   )
 }
