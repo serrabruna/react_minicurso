2713f5995eb945b84c5bd2813f78bc900c78e6e4
serrabruna <brunaibserra@hotmail.com>
2025-10-17 21:18:40 -0300

    junção searchBar e lista

--- projeto-react/src/pages/conferencia/ConferenciaPage.tsx
+++ projeto-react/src/pages/conferencia/ConferenciaPage.tsx
@@ -1,16 +1,26 @@
 import './ConferenciaPage.css'
 import SearchInput from "../../components/SearchInput";
 import { useState } from "react";
+import Lista from '../../components/lista/Lista';
 
 function ConferenciaPage() {
   const [searchText, setSearchText] = useState("");
 
   return (
     <>
-      <SearchInput
-        value={searchText}
-        onChange={(newText) => setSearchText(newText)}
-      />
+    <div className='conference-container'>
+        <div className='barra-de-pesquisa'>
+            <h2 className='page-title'>WishList - Viagens</h2>
+        </div>
+            <SearchInput
+            value={searchText}
+            onChange={(newText) => setSearchText(newText)}
+            />
+
+        <div className='lista-container'>
+            <Lista />
+        </div>
+    </div>
     </>
   );
 }
